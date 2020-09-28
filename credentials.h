e8ceb91c64db684d492c73c870d85d81e1c28dee
Sean Yong <46567682+vjsyong@users.noreply.github.com>
2020-09-29 01:33:27 +0800

    Add files via upload

---
+++ credentials.h
@@ -0,0 +1,6 @@
+//WiFi Credentials are placed here to make sure you don't leak your wifi password when sharing code
+
+const int numberOfNetworks = 3; //YOU MUST MODIFY THIS NUMBER IF YOU ADD/REMOVE NETWORKS
+const char* ssid[] = {"placeholder1\0", "placeholder1\0", "placeholder1\0"};
+const char* password[] = {"password1\0", "password2(\0", "password2\0"};
+//YOU MUST ADD A \0 to the end of each string
